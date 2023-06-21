a4a3564ba0d0974c5f0ac3995d15cfeb1003816a
Jay Aher <jaysureshaher@gmail.com>
2023-06-21 14:12:15 +0530

    Week - 2 Done

---
+++ Lec - 1.R
@@ -0,0 +1,5 @@
+# View(penguins)
+
+# Here's an example of a variable
+first_variable <- "This is my variable"
+second_variable <- 12.5
---
+++ Sales_Calc.R
@@ -0,0 +1,5 @@
+# our first calculation
+quater_1_sales <- 35657.98
+quater_2_sales <- 43810.55
+midyear_sales <- quater_1_sales + quater_2_sales
+yearend_sales <- midyear_sales * 2
